ab88b136952d44e1203f622d49ed87f105c7f423
Rizbi ahmmad <contact.rizbi123@gmail.com>
2026-05-08 01:23:52 +0600

    Adding Cloudinary and Multer for image upload

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,49 @@
-import express, { Application, Request, Response } from "express";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { toNodeHandler } from "better-auth/node";
+import cookieParser from "cookie-parser";
 import cors from "cors";
+import express, { Application, Request, Response } from "express";
+import qs from "qs";
+import { envVars } from "./app/config/env";
+import { auth } from "./app/lib/auth";
+import { IndexRoutes } from "./app/routes";
+import { requestLogger } from "./app/middleware/requestLogger";
 
 const app: Application = express();
 
-// parsers
+app.set("query parser", (str: string) => qs.parse(str));
+
+app.use(requestLogger);
+
+// Global request logger for debugging
+// app.use((req, res, next) => {
+//   console.log(`🚀 [${req.method}] ${req.url}`);
+//   next();
+// });
+
+app.use(
+  cors({
+    origin: [
+      envVars.FRONTEND_URL,
+      envVars.BETTER_AUTH_URL,
+      "http://localhost:3000",
+      "http://localhost:5000",
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
+app.use("/api/auth", toNodeHandler(auth));
+
+// 3. General Parsers (These must come AFTER the /webhook route)
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+app.use(cookieParser());
 
 // application routes
-// app.use('/api/v1', router);
+app.use("/api/v1", IndexRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello from Next Bazar Server!");
--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -7,26 +7,32 @@ dotenv.config();
 interface EnvConfig {
   NODE_ENV: string;
   PORT: string;
+  FRONTEND_URL: string;
   DATABASE_URL: string;
   BETTER_AUTH_SECRET: string;
   BETTER_AUTH_URL: string;
+  CLOUDINARY: {
+    CLOUDINARY_CLOUD_NAME: string;
+    CLOUDINARY_API_KEY: string;
+    CLOUDINARY_API_SECRET: string;
+  };
 }
 
 const loadEnvVariables = (): EnvConfig => {
   const requiredEnvVariable = [
     "NODE_ENV",
     "PORT",
+    "FRONTEND_URL",
     "DATABASE_URL",
     "BETTER_AUTH_SECRET",
     "BETTER_AUTH_URL",
+    "CLOUDINARY_CLOUD_NAME",
+    "CLOUDINARY_API_KEY",
+    "CLOUDINARY_API_SECRET",
   ];
 
   requiredEnvVariable.forEach((variable) => {
     if (!process.env[variable]) {
-      // throw new Error(
-      //   `Environment variable ${variable} is required but not defined in .env file.`,
-      // );
-
       throw new AppError(
         status.INTERNAL_SERVER_ERROR,
         `Environment variable ${variable} is required but not set in .env file.`,
@@ -37,9 +43,15 @@ const loadEnvVariables = (): EnvConfig => {
   return {
     NODE_ENV: process.env.NODE_ENV as string,
     PORT: process.env.PORT as string,
+    FRONTEND_URL: process.env.FRONTEND_URL as string,
     DATABASE_URL: process.env.DATABASE_URL as string,
     BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET as string,
     BETTER_AUTH_URL: process.env.BETTER_AUTH_URL as string,
+    CLOUDINARY: {
+      CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME as string,
+      CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY as string,
+      CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET as string,
+    },
   };
 };
 
